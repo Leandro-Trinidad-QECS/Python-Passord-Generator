3f6403bc7d0a39600cac704b7683c01f6067d6a7
Leandro <l.trinidad@share.epsb.ca>
2018-03-08 06:46:08 -0700

    added a seed section

--- password-generator.py
+++ password-generator.py
@@ -1,26 +1,6 @@
 # !/usr/bin/python
 import random, string
 
-# def generate_passwords(password_length,add_special_characters,seed):
-#     randomSystem = random.SystemRandom()
-#
-#
-#     seed_lowercase_letters = string.ascii_lowercase
-#     seed_uppercase_letters = string.ascii_uppercase
-#     seed_numbers = string.digits
-#     seed_special_char = string.punctuation
-#
-#     if(seed == None):
-#         if(add_special_characters):
-#             pass_seed = seed_lowercase_letters + seed_uppercase_letters + seed_numbers + seed_special_char
-#             output_password = str().join(randomSystem.choice(pass_seed) for _ in range(password_length))
-#         else:
-#             pass_seed = seed_lowercase_letters + seed_uppercase_letters + seed_numbers
-#             output_password = str().join(randomSystem.choice(pass_seed) for _ in range(password_length))
-#
-#     # output_password = str().join(randomSystem.choice(alphabet) for _ in range(password_length))
-#     return output_password;
-
 
 def generate_password(password_length,add_lowerCase,add_upperCase,add_numbers,add_specialChar,seed):
     randomSystem = random.SystemRandom()
@@ -34,7 +14,7 @@ def generate_password(password_length,add_lowerCase,add_upperCase,add_numbers,ad
     #this is what the machine randomy chooses
     passChain = ""
 
-    if(seed == None):
+    if(seed == None or seed == ""):
         if(add_lowerCase):
             passChain += seed_lowercase_letters
         if(add_upperCase):
@@ -44,12 +24,20 @@ def generate_password(password_length,add_lowerCase,add_upperCase,add_numbers,ad
         if(add_specialChar):
             passChain += seed_special_char
     else:
-        passChain = seed
+        # if you want only certain characters it sets the passChain to the
+        # seed that you wanted
+        passChain += seed.lower() if add_lowerCase else ""
+        passChain += seed.upper() if add_upperCase else ""
+        passChain += seed_numbers if add_numbers else ""
+        passChain += seed_special_char if add_specialChar else ""
+
+        # if (not add_lowerCase or not add_upperCase or not add_numbers or not add_specialChar) else ""
 
 
 
+    # randomly chooses from the passchain
     output_password = str().join(randomSystem.choice(passChain) for _ in range(password_length))
     return output_password
 
-
-print(generate_password(20,True,True,True,True,None))
+# Pass Length, LowerCae?,  UpperCase?, Numbers?, Special Char?, seed
+print(generate_password(7,False,False,False,False,"Leo"))
